86759c9927f3489ab10f7eee038f08d23ad486a0
Kinshuk <kinshuk.ram@gmail.com>
2023-04-17 04:06:09 +0200

    Remove spring boot dep

--- pleo-antaeus-app/build.gradle.kts
+++ pleo-antaeus-app/build.gradle.kts
@@ -1,9 +1,6 @@
 plugins {
     application
-    id("org.springframework.boot") version "2.2.4.RELEASE"
-    id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm")
-    kotlin("plugin.spring") version "1.3.70"
 }
 
 kotlinProject()
@@ -19,6 +16,4 @@ dependencies {
     implementation(project(":pleo-antaeus-rest"))
     implementation(project(":pleo-antaeus-core"))
     implementation(project(":pleo-antaeus-models"))
-    implementation(project(":pleo-antaeus-scheduler"))
-    implementation("org.springframework.boot:spring-boot-starter")
 }
--- pleo-antaeus-scheduler/build.gradle.kts
+++ pleo-antaeus-scheduler/build.gradle.kts
@@ -1,9 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "2.2.4.RELEASE"
-    id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm")
-    kotlin("plugin.spring") version "1.3.70"
-
 }
 
 kotlinProject()
@@ -11,5 +7,4 @@ kotlinProject()
 dependencies {
     implementation(project(":pleo-antaeus-core"))
     implementation(project(":pleo-antaeus-models"))
-    implementation("org.springframework.boot:spring-boot-starter")
 }

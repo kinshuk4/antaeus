ddb1bb473a6ad121d799b1c4a0ef80f05f426932
Kinshuk <kinshuk.ram@gmail.com>
2023-04-20 14:43:46 +0200

    fix: Re-add the scheduler part missing from the scheduler

--- pleo-antaeus-scheduler/src/main/kotlin/io/pleo/antaeus/scheduler/FirstOfMonthBillScheduler.kt
+++ pleo-antaeus-scheduler/src/main/kotlin/io/pleo/antaeus/scheduler/FirstOfMonthBillScheduler.kt
@@ -3,6 +3,8 @@ package io.pleo.antaeus.scheduler
 import io.pleo.antaeus.core.messaging.MessageProducer
 import io.pleo.antaeus.core.messaging.Topics
 import io.pleo.antaeus.core.services.InvoiceService
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.util.factory.SchedulerFactory
 import mu.KotlinLogging
 
 private val logger = KotlinLogging.logger {}
@@ -11,9 +13,16 @@ private const val cronExpr = "*/5 * * * *"
 
 class FirstOfMonthBillScheduler(private val invoiceService: InvoiceService, private val messageProducer: MessageProducer) {
     fun start() {
+        SchedulerFactory.createScheduledTask(cronExpr, func = {
+            requestToPayInvoice()
+        })
+    }
+    fun requestToPayInvoice() {
         logger.info("Requesting the bills on 1st of the month")
-        invoiceService.fetchPending().map {
-                invoice -> messageProducer.sendMessage(Topics.pendingInvoices, invoice.id, invoice.id.toString())
+        invoiceService.fetchPending().map { invoice ->
+            logger.info("Requesting invoice '$invoice.id")
+            messageProducer.sendMessage(Topics.pendingInvoices, invoice.id, invoice.id.toString())
+            invoiceService.updateStatusById(invoice.id, InvoiceStatus.REQUESTED)
         }
 
     }
